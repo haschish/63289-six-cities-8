60b4adde09d82c60bd2b808e16609972f6b2fe4d
haschish <haschish@gmail.com>
2021-10-05 01:39:44 +0500

    added main components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../main-page/main-page';
+
+type AppProps = {
+  numberOfOffers: number,
+}
+
+function App(props: AppProps): JSX.Element {
+  return <MainPage numberOfOffers={props.numberOfOffers}/>;
 }
 
 export default App;
