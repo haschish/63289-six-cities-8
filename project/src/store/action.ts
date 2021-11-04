1d3d52a93e784e88fd5902f184169f685e5719b4
haschish <haschish@gmail.com>
2021-11-05 01:05:04 +0500

    fix declaration store/action

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -5,14 +5,14 @@ import { Sort } from '../types/sort';
 export const changeCity = (city: string): ChangeCityAction => ({
   type: ActionType.ChangeCity,
   payload: city,
-});
+} as const);
 
 export const changeSort = (sort: Sort): ChangeSortAction => ({
   type: ActionType.ChangeSort,
   payload: sort,
-});
+} as const);
 
 export const hoverHotel = (hotel?: Hotel): HoverHotelAction => ({
   type: ActionType.HoverHotel,
   payload: hotel,
-});
+} as const);
